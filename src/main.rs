fc7a7f421645d5ad8ffe77549112558c9157c713
Stephen Becker IV <github@deathbyescalator.com>
2015-12-31 12:07:16 -0800

    test reading from file.
    clearly they dont pass

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate bit_vec;
-fn largest_bit(num: &u64) -> Option<usize> {
+
+fn largest_bit(num: &i64) -> Option<usize> {
     if *num == 0 {
         return None;
     } else {
@@ -15,12 +16,12 @@ fn largest_bit(num: &u64) -> Option<usize> {
         if bit > 0 - 1 {
             Some(bit as usize)
         } else {
-            println!("Neg");
+            // println!("Neg");
             None
         }
     }
 }
-fn option_to_bit(bool_bit: &Option<bool>) -> u64 {
+fn option_to_bit(bool_bit: &Option<bool>) -> i64 {
     match *bool_bit {
         Some(b) => {
             if b {
@@ -32,23 +33,24 @@ fn option_to_bit(bool_bit: &Option<bool>) -> u64 {
         None => 0,
     }
 }
-fn xsort(vec: &mut Vec<u64>) -> Option<u64> {
+fn xsort(vec: &mut Vec<i64>) -> Option<i64> {
     let mut empty: Vec<Option<bool>> = vec![None; 32];
-    println!("{}", empty.len());
+    // println!("{}", empty.len());
     for i in 0..vec.len() - 1 {
         let n = vec[i] ^ vec[i + 1];
         let large = largest_bit(&n);
         match large {
             Some(largest) => {
                 let bit_flag = vec[i] & (1 << largest) != 0;
-                println!("{}, {}, {}, {}, {} ",
-                         vec[i],
-                         vec[i + 1],
-                         n,
-                         largest,
-                         bit_flag);
+                // println!("{}, {}, {}, {}, {} ",
+                // vec[i],
+                // vec[i + 1],
+                // n,
+                // largest,
+                // bit_flag);
+                //
                 if empty[largest].is_some() && empty[largest].unwrap() != bit_flag {
-                    println!("break {:?}\n{},{} ", empty, largest, bit_flag);
+                    // println!("break {:?}\n{},{} ", empty, largest, bit_flag);
                     return None;
                 } else {
                     empty[largest] = Some(bit_flag);
@@ -57,10 +59,8 @@ fn xsort(vec: &mut Vec<u64>) -> Option<u64> {
             None => println!("skip"),
         }
     }
-    println!("{:?}", empty);
-    let mut answer: u64 = 0;
-    let mut count = 0;
-    let answer: u64 = empty.iter().rev().fold(0, |acc, &b| acc * 2 + option_to_bit(&b));
+    // println!("{:?}", empty);
+    let answer: i64 = empty.iter().rev().fold(0, |acc, &b| acc * 2 + option_to_bit(&b));
     Some(answer)
 }
 
@@ -70,4 +70,38 @@ fn main() {
     println!("{:?},", xsort(&mut t));
     println!("next");
     println!("{:?},", xsort(&mut t2));
+}
+
+fn load_test(path: std::path::PathBuf) -> (Option<i64>, Vec<i64>) {
+    use std::env;
+    use std::path::Path;
+    use std::io::prelude::*;
+    use std::fs::File;
+    let mut open_file = File::open(path).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    let mut numbers = buffer.lines()
+                            .map(|num| num.trim().parse::<i64>().unwrap())
+                            .collect::<Vec<i64>>();
+
+    let file_result = numbers.remove(0);
+    let result = if file_result >= 0 {
+        Some(file_result)
+    } else {
+        None
+    };
+    (result, numbers)
+
+}
+#[test]
+fn it_works() {
+    use std::fs;
+    let paths = fs::read_dir("./examples").unwrap();
+    for path in paths {
+        let real_path = path.unwrap().path();
+        println!("File: {}", real_path.display());
+        let (result, mut vec) = load_test(real_path);
+        assert_eq!(result, xsort(&mut vec));
+    }
+    assert!(true);
 }
